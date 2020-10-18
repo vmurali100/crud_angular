88f57cd38e0b20f20e732dc4d12dda2deab2a69f
vmurali100 <vmurali100@gmail.com>
2020-10-18 15:30:22 +0530

    Creating Module Driven Form And Template Driven Form

---
+++ crud-app/src/app/mdf/mdf.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+
+@Component({
+  selector: 'app-mdf',
+  templateUrl: './mdf.component.html',
+  styleUrls: ['./mdf.component.css']
+})
+export class MdfComponent implements OnInit {
+
+  userForm=new FormGroup({
+    fname:new FormControl()
+  })
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+  addUser(){
+    console.log(this.userForm.value)
+  }
+}
